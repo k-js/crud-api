51805f99ecec470da6be801c6dbf9bae732ba617
alebulka <bulkalessandra@outlook.com>
2022-07-04 17:05:12 -0300

    Criando atributos no planos-models

---
+++ src/models/planos-models.js
@@ -0,0 +1,13 @@
+let id = 0;
+
+class Plano {
+    constructor(preco, cargo, status_servico, horarios, salario){
+        this.id_preco = id++;
+        this.preco = preco;
+        this.id_cadastro_cliente = id_cadastro_cliente;
+        this.qtd_meses = qtd_meses;
+        this.carac = carac;
+    }
+}
+
+export {Plano}

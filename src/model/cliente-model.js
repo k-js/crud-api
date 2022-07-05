7b6a761b2ca05e12ef17bd54434834971dbc7dca
keu santos <modulodigital69@gmail.com>
2022-07-05 10:48:56 -0300

    modelo

---
+++ src/model/cliente-model.js
@@ -0,0 +1,12 @@
+class Clientes{
+    constructor(nome, email, telefone, endereco,dataNascimento, cpf){
+        this.nome = nome;
+        this.email=email;
+        this.telefone=telefone;
+        this.endereco=endereco;
+        this.dataNascimento=dataNascimento
+        this.cpf=cpf
+    }
+}
+
+export {Clientes}

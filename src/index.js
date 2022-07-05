d2a89c7a5e3f33b747d554455cf1a02428367d58
keu santos <modulodigital69@gmail.com>
2022-07-05 11:52:17 -0300

    ajust

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ app.use(express.json());
 
 import { bdSQLite } from "./infra/sqlite.js";
 
-const funcionario = require('../crud-api/src/controller/funcionario-controller'); 
-
 import { cliente } from "./controller/cliente-controller.js";
-
 cliente(app, bdSQLite);
+
+
+const funcionario = require('../crud-api/src/controller/funcionario-controller'); 
 funcionario(app, bdSQLite);
 
 app.listen(3000, () => { console.log('RODANDO NA PORTA 3000')})

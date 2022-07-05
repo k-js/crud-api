8c269b60f376d4e17d8fd1d5719959f6801f2359
keu santos <modulodigital69@gmail.com>
2022-07-05 18:48:54 -0300

    ref do bd

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"; 
+const app=express(); 
+
+app.use(express.json());
+
+
+import { bdSQLite } from "./src/infra/sqlite.js";
+
+import { cliente } from "./src/controller/cliente-controller.js";
+cliente(app, bdSQLite);
+
+// import { funcionario } from "./src/controller/funcionario-controller.js";
+// funcionario(app, bdSQLite);
+
+app.listen(3000, () => { console.log('RODANDO NA PORTA 3000')})
--- src/infra/sqlite.js
+++ src/infra/sqlite.js
@@ -1,11 +1,11 @@
-import { sqlite3 } from "sqlite3";
-const bd = new sqlite3.Database('./src/infra/database.db');
+import  sqlite3  from 'sqlite3';
+const bdSQLite = new sqlite3.Database('./src/infra/database.db');
 
 process.on('SIGINT', () =>
-    bd.close(() => {
+    bdSQLite.close(() => {
         console.log('BD encerrado!');
         process.exit(0);
     })
 );
 
-export {bd} ;
+export {bdSQLite} ;

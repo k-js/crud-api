0aa4f230534a83050b68a705b47ff20ee44a54b0
keu santos <modulodigital69@gmail.com>
2022-07-05 10:49:34 -0300

    modificando importacoes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import express from "express"; 
 const app=express(); 
+
+app.use(express.json());
+
+import { bdSQLite } from "./infra/sqlite";
+
+import { cliente } from "./controller/cliente-controller.js";
+cliente(app, bdSQLite);
+
+app.listen(3000, () => { console.log('RODANDO NA PORTA 3000')})

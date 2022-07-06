ab66e6f2be3c7b87f64e97d589658f9c3fed32d2
alebulka <bulkalessandra@outlook.com>
2022-07-06 14:41:12 -0300

    model do planos agora na branch

--- src/models/planos-models.js
+++ src/models/planos-models.js
@@ -1,13 +1,13 @@
-// let id = 0;
+let id = 0;
 
-// class Plano {
-//     constructor(preco, cargo, status_servico, horarios, salario){
-//         this.id_preco = id++;
-//         this.preco = preco;
-//         this.id_cadastro_cliente = id_cadastro_cliente;
-//         this.qtd_meses = qtd_meses;
-//         this.carac = carac;
-//     }
-// }
+class Plano {
+    constructor(preco, cargo, status_servico, horarios, salario){
+        this.id_preco = id++;
+         this.preco = preco;
+         this.id_cadastro_cliente = id_cadastro_cliente;
+         this.qtd_meses = qtd_meses;
+         this.carac = carac;
+    }
+}
 
-// export {Plano}
+export {Plano};

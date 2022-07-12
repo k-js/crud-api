8537374a5b2a5cc4badd3fa67d7e6eb1f359bd01
BrunaRavelli-byte <brunaravelli13@gmail.com>
2022-07-11 22:09:28 -0300

    Implementação de entidades no método Rest

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import express from "express"; 
-const app=express(); 
+
+
+
+
+
+
+
+const customExpress = require("./src/config/customExpress");
+const conexao = require("./src/infrastructure/conexao");
+const Tabelas = require("./src/infrastructure/tabelas");
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    console.log("Conectado com sucesso!!");
+    Tabelas.init(conexao);
+    const app = customExpress();
+    app.listen(2000, () => console.log("servidor rodando na porta 2000"));
+  }
+});
